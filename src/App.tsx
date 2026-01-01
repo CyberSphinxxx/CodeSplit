f2f441751081d3b65149566d3eaf2f4303cb117c
CyberSphinxx <johnlemargonzales@gmail.com>
2026-01-02 05:34:37 +0800

    feat: add react-router-dom and configure routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 import Layout from "./components/Layout/Layout";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Overview from "./pages/Overview";
+import Projects from "./pages/Projects";
+import Settings from "./pages/Settings";
+import Community from "./pages/Community";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 function App() {
-  return <Layout />;
+  return (
+    <Routes>
+      {/* Public Route */}
+      <Route path="/" element={<LandingPage />} />
+
+      {/* Dashboard with nested routes */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Overview />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="community" element={<Community />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+
+      {/* Editor Route */}
+      <Route
+        path="/editor/:id?"
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
